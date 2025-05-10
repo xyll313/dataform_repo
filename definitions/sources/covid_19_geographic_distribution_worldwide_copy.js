bf885dcd68aac22565e4897b4e2d44fad00742c9
xiayu <xyll313@gmail.com>
2025-05-11 01:47:50 +0200

    add commas

--- definitions/sources/covid_19_geographic_distribution_worldwide_copy.js
+++ definitions/sources/covid_19_geographic_distribution_worldwide_copy.js
@@ -5,7 +5,7 @@ declare({
         {name: "date", type: "DATE"},
         {name: "daily_deaths", type: "INTEGER"},
         {name: "confirmed_cases", type:"INTEGER"},
-        {name: "contries_and_territories", type: "INTEGER"},
+        {name: "contries_and_territories", type: "STRING"},
         {name: "geo_id", type: "STRING"},
         {name: "country_territory_code", type:"STRING"},
         {name: "pop_data_2019", type:"INTEGER"}
