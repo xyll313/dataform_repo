a9545fe66bbad1372d430f9731310ce6fded740e
xiayu <xyll313@gmail.com>
2025-05-11 01:20:49 +0200

    add declearation

---
+++ definitions/sources/covid_19_geographic_distribution_worldwide_copy.js
@@ -0,0 +1,13 @@
+declear({
+    database: "covid_US",
+    name:  "covid_19_geographic_distribution_worldwide_copy",
+    columns: [
+        {name: "date", type: "DATE"},
+        {name: "daily_deaths", type: "INTEGER"},
+        {name: "confirmed_cases", type:"INTEGER"},
+        {name: "contries_and_territories", type: "INTEGER"},
+        {name: "geo_id", type: "STRING"},
+        {name: "country_territory_code", type:"STRING"},
+        {name: "pop_data_2019", type:"INTEGER"}
+    ]
+})
